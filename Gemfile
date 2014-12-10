2dfd7c9b0caf33ea06e3ebc6f5fe56f1b1325485
amoose <amos@amosarts.com>
2014-12-09 23:10:33 -0800

    Breaks layout in to views

--- Gemfile
+++ Gemfile
@@ -1,8 +1,13 @@
 source 'https://rubygems.org'
-ruby '2.1.2'
+ruby '2.1.5'
 
 gem 'websocket'
 gem 'sinatra'
 gem 'thin'
 gem 'slim'
 gem 'dotenv'
+gem 'websocket-eventmachine-client'
+gem 'json'
+
+gem 'pry'
+gem 'pry-nav'
--- buffalo.rb
+++ buffalo.rb
@@ -1,13 +1,69 @@
 require 'sinatra'
 require 'websocket'
 require 'dotenv'
+require 'slim'
+require 'websocket-eventmachine-client'
+require 'json'
+
+require 'pry'
+require 'pry-nav'
 Dotenv.load
 
-get '/' do
-  @handshake = WebSocket::Handshake::Client.new(url: "ws://#{ENV['HONDOMAIN']}/websocket", headers: { 'Cookie' => 'BUFFALOID=1' })
+module Buffalo
+  module Client
+    @uid = ENV['UID']
+    @subscribed = false
+    def self.initiate_connection
+      puts "  connecting!"
+      WebSocket::EventMachine::Client.connect(:uri => "ws://#{ENV['HONDOMAIN']}/websocket?uid=#{@uid}")
+    end
 
-  # Create request
-	@handshake.to_s
+    def self.run
+      EM.run do
+        @ws = Buffalo::Client.initiate_connection
 
-	@handshake.inspect
+        @ws.onopen do
+          puts "  * client connected."
+        end
+
+        @ws.onmessage do |msg, type|
+          puts "Received message: #{msg}"
+          json = JSON.parse(msg.to_s).first
+          if json.first == 'authorization_token'
+            @token = json[1]['data']['value']
+            @ws.send [:identify, { uid: @uid, token: @token } ].to_json
+            @ws.send [ 'websocket_rails.subscribe', { data: { channel: 'buffalo', user_id: @uid } } ].to_json
+          elsif json.first == 'websocket_rails.channel_token'
+            @channel_token ||= json[1]['data']['token']
+            @subscribed = true
+            @ws.send [ 'websocket_rails.subscribe', { channel: 'buffalo', data: { token: @channel_token, user_id: @uid } } ].to_json
+          elsif json.first == 'websocket_rails.ping'
+            @ws.send [ 'websocket_rails.pong', { id: nil } ].to_json
+          end
+        end
+
+        @ws.onclose do |code, reason|
+          puts "Disconnected with status code: #{code}."
+          puts "  sleeping..."
+          @ws.close
+          
+          sleep(2)
+          EM.stop
+          exit
+          Buffalo::Client.run
+        end
+      end
+    end
+  end
 end
+
+
+
+
+get '/' do
+  @handshake = nil
+  # @handshake.to_s
+  slim :index
+end
+
+Buffalo::Client.run
